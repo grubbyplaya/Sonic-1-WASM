f662e8d3296f1f1458f2c4e684936284a29b2ace
grubbyplaya <91162025+grubbyplaya@users.noreply.github.com>
2022-05-08 22:11:39 -0400

    Add files via upload

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -82,16 +82,16 @@ self.addEventListener("fetch", (event) => {
   }
 
  // Change this to your repository name
-var GHPATH = '/Sonic-1-WASM';
+var GHPATH = 'Sonic-1-WASM';
  
 // Choose a different app prefix name
-var APP_PREFIX = 'sonicx_';
+var APP_PREFIX = 'tailstube_';
  
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_02';
+var VERSION = 'version_01';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
